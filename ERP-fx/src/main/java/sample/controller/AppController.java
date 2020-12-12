dd5a4a8b4428f8901857b2d6df39ea3989bda818
Bartlomiej Calko <bartlomiej.calko@gmail.com>
2020-12-13 00:43:46 +0100

    added tables in app.fxml and EmployeeTabelModel

--- ERP-fx/src/main/java/sample/controller/AppController.java
+++ ERP-fx/src/main/java/sample/controller/AppController.java
@@ -1,8 +1,13 @@
 package sample.controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import sample.table.EmployeeTableModel;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -10,10 +15,34 @@ import java.util.ResourceBundle;
 public class AppController implements Initializable {
 
     @FXML
-    private TableView<?> employeeTableView;
+    private TableView<EmployeeTableModel> employeeTableView;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         employeeTableView.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+
+        TableColumn firstNameColumn = new TableColumn("First Name");
+        firstNameColumn.setMinWidth(100);
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<EmployeeTableModel, String>("firstName"));
+
+        TableColumn lastNameColumn = new TableColumn("Last Name");
+        lastNameColumn.setMinWidth(100);
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<EmployeeTableModel, String>("lastName"));
+
+        TableColumn salary = new TableColumn("Salary");
+        salary.setMinWidth(100);
+        salary.setCellValueFactory(new PropertyValueFactory<EmployeeTableModel, String>("salary"));
+
+        employeeTableView.getColumns().addAll(firstNameColumn, lastNameColumn, salary);
+
+        ObservableList<EmployeeTableModel> data = FXCollections.observableArrayList();
+
+        data.add(new EmployeeTableModel("barto", "calko", "50000"));
+        data.add(new EmployeeTableModel("git", "hub", "30000"));
+        data.add(new EmployeeTableModel("emi", "mickey", "100000"));
+
+        employeeTableView.setItems(data);
+
+
     }
 }
